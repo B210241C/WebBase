a57846549bd8680beeef1fa5ba4a575b6d9d2bab
AlexTan115 <B210242C@sc.edu.my>
2022-01-26 23:20:00 +0800

    allproduct

--- resources/views/allProduct.blade.php
+++ resources/views/allProduct.blade.php
@@ -3,32 +3,18 @@
 <div class="row justify-content-center">
     @if($products)
         @foreach($products as $product)
-        <div class="row">
-            <div class="col-sm-3"></div>
-                <div class="col-sm-6">
-                   <br><br>
-                     <div class="card-body">
-                         <div class="row">
-                              <div class="col-md-6">
-                            
-                                    <img src="{{ asset('images/') }}/{{$product->image}}" alt="" width="100%" class="card-image-top">
-                                           <div class="card-body">
-                                              <h5 class="card-title">{{$product->name}}</h5>
-                                              <p class="card-text">RM{{$product->price}}</p>
-                                              <a href="{{ route('productdetail',['id'=>$product->id])}}" target="_blank" class="btn btn-danger">Add to cart</a>
-                                           
-                                 </div>
-                             </div>
-                         </div>
-                    </div>
+            <div class="card m-1"  style="width: 33%;">
+                <img src="{{ asset('images/') }}/{{$product->image}}" alt="" width="70%" class="card-image-top">
+                <div class="card-body">
+                    <h5 class="card-title">{{$product->name}}</h5>
+                    <p class="card-text">{{$product->price}}</p>
+                    <a href="{{ route('productdetail',['id'=>$product->id])}}" target="_blank" class="btn btn-danger">Add to cart</a>
                 </div>
-            <div class="col-sm-3"></div>
+            </div>
         @endforeach
         @endif
-
 </div>
 <div class="d-flex justify-content-center">
     {!! $products->links() !!}
-</div>
 </div>
 @endsection
