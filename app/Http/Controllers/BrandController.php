f7f4ada9cfdafe7ea3b945c737cc80107df6500f
AlexTan115 <B210242C@sc.edu.my>
2022-01-28 04:26:25 +0800

    delete

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Session;
 use Illuminate\Http\Request;
 use DB;
 use App\Models\brands;
@@ -19,5 +19,13 @@ class BrandController extends Controller
     public function view(){
     	$viewBrand=brands::all();//generate SQL select*from category
     	return view('showBrand')->with('brands',$viewBrand);
+    }
+  
+
+    public function delete($id){
+        $deleteProduct=brands::find($id);
+        $deleteProduct->delete();
+        Session::flash('success',"Brand delete sucessfully!");
+        return redirect()->route('viewBrand');
     }
 }
